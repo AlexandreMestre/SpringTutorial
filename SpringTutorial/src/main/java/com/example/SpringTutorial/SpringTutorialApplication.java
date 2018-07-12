ea7ee524e4db9e8facf0c4b952eec6a767faa1ef
Alexandre Mestre <alexandre.conti.mestre@gmail.com>
2018-07-12 14:34:14 -0300

    Conflict resolution

---
+++ SpringTutorial/src/main/java/com/example/SpringTutorial/SpringTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialApplication.class, args);
+	}
+}
