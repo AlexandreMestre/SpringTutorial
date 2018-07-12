ef864d1aa3170322d9b6127de22301648b6f44d2
Alexandre Mestre <alexandre.conti.mestre@gmail.com>
2018-07-12 14:30:51 -0300

    Conflicts resolution

--- SprintTutorial/src/main/java/com/example/SpringTutorial/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringTutorial.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.SpringTutorial.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
--- SprintTutorial/src/main/java/com/example/SpringTutorial/repository/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringTutorial.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.SpringTutorial.model.Teacher;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-
-}
